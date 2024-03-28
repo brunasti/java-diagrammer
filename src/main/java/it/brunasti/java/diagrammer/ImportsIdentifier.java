446fe62e587f530f13c7bf90393b68901f781ef7
brunasti <paolo@brunasti.it>
2024-03-28 13:43:36 +0100

    Adds tests for ImportsIdentifier

--- src/main/java/it/brunasti/java/diagrammer/ImportsIdentifier.java
+++ src/main/java/it/brunasti/java/diagrammer/ImportsIdentifier.java
@@ -20,6 +20,8 @@ public class ImportsIdentifier {
   public static final String FILE_TYPE = ".java";
 
   public Set<String> extractImports(String path, String sysPath) {
+    Main.debug(4, "ImportsIdentifier.extractImports : " + path + " - " + sysPath);
+
     Set<String> importFiles = new HashSet<>();
     // TODO : Could even extract packages....
     //    Set<String> packages = new HashSet<>();
@@ -31,17 +33,20 @@ public class ImportsIdentifier {
       Collection<JavaSource> srcs = jp.getSources();
 
       for (JavaSource src : srcs) {
+        Main.debug(8, "ImportsIdentifier.extractImports : src " + src);
         //        packages.add(src.getPackage().toString());
 
         for (String imprt : src.getImports()) {
+          Main.debug(5, "ImportsIdentifier.extractImports : imprt " + imprt);
           // TODO: could exclude imports with a test like : if (imprt.startsWith("")) {
           try {
             if (importFiles.contains(imprt)) {
               continue;
             }
             importFiles.add(imprt);
-            imprt = sysPath + imprt.replaceAll("\\.", "/") + FILE_TYPE;
-            extractImports(imprt, sysPath);
+            String newImprt = sysPath + imprt.replaceAll("\\.", "/") + FILE_TYPE;
+            Main.debug(6, "ImportsIdentifier.extractImports : newImprt " + newImprt);
+            extractImports(newImprt, sysPath);
           } catch (Exception ex) {
             Main.debug(4, "  Error ImportsIdentifier.extractImports : "
                     + ex.getMessage() + " = " + path + " - " + imprt);
--- src/test/java/it/brunasti/java/diagrammer/ImportsIdentifierTest.java
+++ src/test/java/it/brunasti/java/diagrammer/ImportsIdentifierTest.java
@@ -1,11 +1,50 @@
 package it.brunasti.java.diagrammer;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import it.brunasti.java.diagrammer.teststructure.TestBaseClass;
 import org.junit.jupiter.api.Test;
 
+import java.util.Set;
+
 // TODO: implement the test cases for this class
-class ImportsIdentifierTest {
+class ImportsIdentifierTest implements TestConstants {
 
   @Test
-  void extractImports() {
+  void extractImports_errors() {
+    ImportsIdentifier importsIdentifier = new ImportsIdentifier();
+    String newSysPath = testJavaSrcDirectory + '/';
+    assertDoesNotThrow(() -> importsIdentifier.extractImports(javaSrcDirectory, ""));
+
+    String javaClassName = newSysPath
+            + this.getClass().getName().replace(".", "/")
+            + ImportsIdentifier.FILE_TYPE;
+    assertDoesNotThrow(() -> importsIdentifier.extractImports(javaClassName, newSysPath));
+    Main.setDebug(10);
+    Set<String> imports = importsIdentifier.extractImports(javaClassName, newSysPath);
+    Main.debug("  - imports : " + imports);
+    Main.debug("  - imports : " + imports.size());
+    assertEquals(5, imports.size());
   }
+
+
+  @Test
+  void extractImports_testStructureClasses() {
+    Main.setDebug(7);
+    Main.debug("extractImports_testStructureClasses ---------");
+    ImportsIdentifier importsIdentifier = new ImportsIdentifier();
+    String className = TestBaseClass.class.getCanonicalName();
+    Main.debug(" - " + className);
+    String newSysPath = testJavaSrcDirectory + '/';
+    String javaClassName = newSysPath
+            + className.replace(".", "/")
+            + ImportsIdentifier.FILE_TYPE;
+    Set<String> imports = importsIdentifier.extractImports(javaClassName, newSysPath);
+    Main.debug("  - imports : " + imports);
+    Main.debug("  - imports : " + imports.size());
+    assertEquals(1, imports.size());
+  }
+
+
 }
