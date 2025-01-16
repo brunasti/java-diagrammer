7b54af592f28afd291f8f9ad69ddab6212522e18
brunasti <paolo@brunasti.it>
2025-01-16 15:04:28 +0100

    Tadaah more diagrams

--- src/test/java/it/brunasti/java/diagrammer/AdHocDiagramGenerationTest.java
+++ src/test/java/it/brunasti/java/diagrammer/AdHocDiagramGenerationTest.java
@@ -15,6 +15,7 @@ public class AdHocDiagramGenerationTest implements TestConstants {
   void testMainPublicMethods() {
     Options options = new Options();
 
+    // Commons
     System.err.println("ClassDiagrammerMain.main ------ for Tadaah projects -----");
     String[] fullArgs = new String[9];
     fullArgs[0] = "/Users/paolo/Work/Tadaah/fp-backend-commons/fp-commons/build/classes/java/main";
@@ -24,10 +25,56 @@ public class AdHocDiagramGenerationTest implements TestConstants {
     fullArgs[4] = "/Users/paolo/IdeaProjects/mine/java-diagrammer/temp/tadaah/config.json";
     fullArgs[5] = "-o";
     fullArgs[6] = "/Users/paolo/IdeaProjects/mine/java-diagrammer/temp/tadaah/Tadaah-fp-backend-commons-fp-commons.puml";;
-//    fullArgs[7] = "-p";
-//    fullArgs[8] = classesDirectory;
     fullArgs[7] = "-i";
     fullArgs[8] = "/Users/paolo/Work/Tadaah/fp-backend-commons/fp-commons/src/main/java";
+    assertDoesNotThrow(() -> ClassDiagrammerMain.main(fullArgs));
+
+    // Authemntication
+    fullArgs[0] = "/Users/paolo/Work/Tadaah/fp-backend-commons/fp-authentication/build/classes/java/main";
+    fullArgs[1] = "/Users/paolo/IdeaProjects/mine/java-diagrammer/temp/tadaah/Tadaah-fp-backend-commons-fp-autentication";
+    fullArgs[2] = "-d";
+    fullArgs[3] = "-c";
+    fullArgs[4] = "/Users/paolo/IdeaProjects/mine/java-diagrammer/temp/tadaah/config.json";
+    fullArgs[5] = "-o";
+    fullArgs[6] = "/Users/paolo/IdeaProjects/mine/java-diagrammer/temp/tadaah/Tadaah-fp-backend-commons-fp-autentication.puml";;
+    fullArgs[7] = "-i";
+    fullArgs[8] = "/Users/paolo/Work/Tadaah/fp-backend-commons/fp-autentication/src/main/java";
+    assertDoesNotThrow(() -> ClassDiagrammerMain.main(fullArgs));
+
+    // Invoice
+    fullArgs[0] = "/Users/paolo/Work/Tadaah/fp-backend-commons/fp-commons-invoice/build/classes/java/main";
+    fullArgs[1] = "/Users/paolo/IdeaProjects/mine/java-diagrammer/temp/tadaah/Tadaah-fp-backend-commons-fp-invoice";
+    fullArgs[2] = "-d";
+    fullArgs[3] = "-c";
+    fullArgs[4] = "/Users/paolo/IdeaProjects/mine/java-diagrammer/temp/tadaah/config.json";
+    fullArgs[5] = "-o";
+    fullArgs[6] = "/Users/paolo/IdeaProjects/mine/java-diagrammer/temp/tadaah/Tadaah-fp-backend-commons-fp-invoice.puml";;
+    fullArgs[7] = "-i";
+    fullArgs[8] = "/Users/paolo/Work/Tadaah/fp-backend-commons/fp-commons-invoice/src/main/java";
+    assertDoesNotThrow(() -> ClassDiagrammerMain.main(fullArgs));
+
+    // Invoice
+    fullArgs[0] = "/Users/paolo/Work/Tadaah/fp-backend-notification-service/build/classes/java/main";
+    fullArgs[1] = "/Users/paolo/IdeaProjects/mine/java-diagrammer/temp/tadaah/Tadaah-fp-backend-notification";
+    fullArgs[2] = "-d";
+    fullArgs[3] = "-c";
+    fullArgs[4] = "/Users/paolo/IdeaProjects/mine/java-diagrammer/temp/tadaah/config.json";
+    fullArgs[5] = "-o";
+    fullArgs[6] = "/Users/paolo/IdeaProjects/mine/java-diagrammer/temp/tadaah/Tadaah-fp-backend-notification.puml";;
+    fullArgs[7] = "-i";
+    fullArgs[8] = "/Users/paolo/Work/Tadaah/fp-backend-notification/src/main/java";
+    assertDoesNotThrow(() -> ClassDiagrammerMain.main(fullArgs));
+
+    // Http-client
+    fullArgs[0] = "/Users/paolo/Work/Tadaah/fp-backend-commons/fp-http-client/build/classes/java/main";
+    fullArgs[1] = "/Users/paolo/IdeaProjects/mine/java-diagrammer/temp/tadaah/Tadaah-fp-backend-commons-fp-http-client";
+    fullArgs[2] = "-d";
+    fullArgs[3] = "-c";
+    fullArgs[4] = "/Users/paolo/IdeaProjects/mine/java-diagrammer/temp/tadaah/config.json";
+    fullArgs[5] = "-o";
+    fullArgs[6] = "/Users/paolo/IdeaProjects/mine/java-diagrammer/temp/tadaah/Tadaah-fp-backend-commons-fp-http-client.puml";;
+    fullArgs[7] = "-i";
+    fullArgs[8] = "/Users/paolo/Work/Tadaah/fp-backend-commons/fp-http-client/src/main/java";
     assertDoesNotThrow(() -> ClassDiagrammerMain.main(fullArgs));
   }
 
